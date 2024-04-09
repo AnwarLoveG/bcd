9804fb32c9c6ab7f09e40e05564770222f576585
Anwar Belkheir <Anwar Belkheir>
2024-04-09 11:42:39 +0200

    Terzo commit

--- bcd.java
+++ bcd.java
@@ -1,5 +1,7 @@
 package verifica;
 
+import java.util.Hashtable;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -9,7 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-	public class bcd extends Application {
+public class bcd extends Application {
 		
 		TextField tNum= new TextField();
 		Label lRis = new Label();
@@ -23,21 +25,54 @@ import javafx.stage.Stage;
 			griglia.add(tNum,0,0);
 			griglia.add(lNum, 1, 0);
 			griglia.add(bCalcola, 0, 2);
-			griglia.add(lBcd, 0, 3);
+			griglia.add(lBcd, 1, 2);
 			griglia.add(lRis, 0, 4);
-			griglia.setHgap(10);
-			griglia.setVgap(10);
+			griglia.setHgap(15);
+			griglia.setVgap(15);
 			griglia.setAlignment(Pos.CENTER);
 			
-			Scene scena = new Scene(griglia);
+			Scene scena = new Scene(griglia ,500,500);
 			finestra.setTitle("Algoritmo BCD");
 			finestra.show();
 			finestra.setScene(scena);
+			
+			bCalcola.setOnAction(e->calcola());
 		}
-		public void main(String []args) {
+		public void calcola() {
+			
+			String vettore[]=tNum.getText().split("");
+			String bcd="";
+			
+			Hashtable <String,String> convertiNum= new Hashtable <String,String>();
+			
+			convertiNum.put("0","0000");
+			
+			convertiNum.put("1","0001");
+			
+			convertiNum.put("2","0010");
+			
+			convertiNum.put("3","0011");
+			
+			convertiNum.put("4","0100");
+			
+			convertiNum.put("5","0101");
+			
+			convertiNum.put("6","0110");
+			
+			convertiNum.put("7","0111");
+			
+			convertiNum.put("8","1000");
+			
+			convertiNum.put("9","1001");
+			
+			
+			for(int i=0;i<vettore.length;i++) {
+				bcd+=convertiNum.get(vettore[i]);
+			}
+			lRis.setText(bcd);
+			
+		}
+		public static void main(String []args) {
 			launch(args);
 		}
 	}
-
-	
-			
